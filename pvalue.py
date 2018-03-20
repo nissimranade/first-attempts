2f36d9cce623d036cd274519669ea86b11dd9abd
nissimranade <36382869+nissimranade@users.noreply.github.com>
2018-03-19 20:00:41 -0400

    more changes

--- pvalue.py
+++ pvalue.py
@@ -14,24 +14,30 @@ def f():
     return abs(random.normalvariate(.2, 7) ** (int(3 * random.random())))
 
 L = []
-for i in range(10000):
+c1 = 0
+for i in range(10**6):
     L.append(i)
     L[i] = f()
+    if L[i]>1000
+        c1 = c1+1
 
 plt.xlim([min(L)-5, max(L)+5])
 plt.hist(L, bins=100, alpha=1)
 plt.show()
 
-K = []
 
-for j in range(10000):
+
+K = []
+c2 = 0
+for j in range(10**6):
     K.append(j)
     K[j] = f()
     for i in range (19):
         y = f()
         if y > K[j]: 
             K[j] = y    
-    
+    c2 = c2 +1
+
 plt.xlim([min(K)-5, max(L)+5])
 plt.hist(K, bins=100, alpha=1)
 plt.show()
